73ad83f59d6674155dcac79724e80e8e827d5f8d
yonko-mo <alsantel.ma@gmail.com>
2026-06-08 14:44:09 +0300

    changed theme ,created notes view and updated analysis options

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -9,9 +10,12 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Notes')),
-      body: Center(child: Text('Welcome to the Notes App!')),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const NotesView(),
     );
   }
 }
