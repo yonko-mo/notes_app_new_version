5f3beb6939fb116461b75fc3a6ca177278857377
yonko-mo <alsantel.ma@gmail.com>
2026-06-08 21:10:37 +0300

    add NoteModel class to represent note data structure

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final DateTime dateTime;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.dateTime,
+    required this.color,
+  });
+}
