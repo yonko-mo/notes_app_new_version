a3e5b0a396b761864a7fa4156c2822bdcfcca6b8
yonko-mo <alsantel.ma@gmail.com>
2026-06-09 20:41:49 +0300

    Add SimpleBlocObserver for enhanced Bloc debugging
    - Implemented SimpleBlocObserver class to provide detailed logging for Bloc lifecycle events including onChange, onClose, onCreate, onDone, onError, onEvent, and onTransition.
    - This observer will help in tracking state changes and errors in the Bloc architecture, improving debugging and maintenance.

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
 
@@ -26,10 +25,12 @@ class AddNoteBottomSheet extends StatelessWidget {
             }
           },
           builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoading ? true : false,
-              child: const AddNoteForm(),
-            );
+            if (state is AddNoteLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return const AddNoteForm();
           },
         ),
       ),
